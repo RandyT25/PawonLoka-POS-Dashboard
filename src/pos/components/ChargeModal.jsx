38bd806b5f49e9ec03f3a38aaede4a7f4185976e
Randy <randy@Randys-MacBook-Pro.local>
2026-05-25 20:46:36 +0800

    fix: ChargeModal bundle receipt format

--- src/pos/components/ChargeModal.jsx
+++ src/pos/components/ChargeModal.jsx
@@ -6,10 +6,9 @@ import { useWhatsApp } from '../hooks/useWhatsApp'
 const formatReceipt = (order, customer) => {
   const items = order.items.map(i => {
     let line = i.qty + 'x ' + i.name + ' - Rp ' + (i.price * i.qty).toLocaleString('id-ID')
-    if (i.isBundle && i.bundleItems) line += '
-' + i.bundleItems.map(b => '  > '+(b.qty>1?b.qty+'x ':'')+b.name+(b.free?' FREE':'')).join('
-')
-' + i.bundleItems.map(b => '  > '+(b.qty>1?b.qty+'x ':'')+b.name+(b.free?' FREE':'')).join('
+    if (i.isBundle && i.bundleItems) line += ' (' + i.bundleItems.map(b => (b.qty>1?b.qty+'x ':'')+b.name+(b.free?' FREE':'')).join(', ') + ')'
+    return line
+  }).join('
 ')
     return line
   }).join('
